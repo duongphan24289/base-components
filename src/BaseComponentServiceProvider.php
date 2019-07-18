5a4f140816b16c247abc104fd70a83ed24d82279
Mr.Phan <duong.pv@neo-lab.vn>
2019-07-18 10:41:36 +0700

    Add source code

---
+++ src/BaseComponentServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MittoPlus\BaseComponent;
+
+use Illuminate\Support\ServiceProvider;
+
+class BaseComponentServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // TODO
+    }
+
+    public function register()
+    {
+        // TODO
+    }
+}
